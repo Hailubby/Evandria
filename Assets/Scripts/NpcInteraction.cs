a529eb8a1a2d10f2b178ab21700de5c19c359d42
Hailubby <hwan561@aucklanduni.ac.nz>
2016-10-03 01:07:55 +1300

    Added NPC and colliders

---
+++ Assets/Scripts/NpcInteraction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NpcInteraction : MonoBehaviour, Assets.Scripts.Interactable
+{
+    public void interact()
+    {
+        Debug.Log("This NPC has been interacted with!");
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
