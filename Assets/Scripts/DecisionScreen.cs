7137594f5316e73733936ac88ac8bd0c5ecc9202
OphielOM <wlin943@aucklanduni.ac.nz>
2016-10-03 17:37:42 +1300

    Prototype Final Decision Screen
    Initial implementation of Final Decision screen. Placeholder panels are
    used for the character profiles. Extra test button is present to
    populate the clues.

---
+++ Assets/Scripts/DecisionScreen.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+public class DecisionScreen : MonoBehaviour {
+    private ModalPanel modalPanel;
+
+    private UnityAction firstCandidate;
+    private UnityAction secondCandidate;
+
+    void Awake()
+    {
+        modalPanel = ModalPanel.Instance();
+
+        firstCandidate = new UnityAction(firstChoice);
+        secondCandidate = new UnityAction(secondChoice);
+    }
+
+    public void decision ()
+    {
+        modalPanel.Choice(firstCandidate, secondCandidate);
+    }
+
+    public void firstChoice ()
+    {
+        Text firstAccept = GameObject.Find("First Accept").GetComponent<Text>();
+        Color firstAcceptColor = firstAccept.color;
+        firstAcceptColor.a = 1;
+        firstAccept.color = firstAcceptColor;
+
+        Text secondReject = GameObject.Find("Second Reject").GetComponent<Text>();
+        Color secondRejectColor = secondReject.color;
+        secondRejectColor.a = 1;
+        secondReject.color = secondRejectColor;
+
+    }
+
+    public void secondChoice ()
+    {
+        Text secondAccept = GameObject.Find("Second Accept").GetComponent<Text>();
+        Color secondAcceptColor = secondAccept.color;
+        secondAcceptColor.a = 1;
+        secondAccept.color = secondAcceptColor;
+
+        Text firstReject = GameObject.Find("First Reject").GetComponent<Text>();
+        Color firstRejectColor = firstReject.color;
+        firstRejectColor.a = 1;
+        firstReject.color = firstRejectColor;
+    }
+}
