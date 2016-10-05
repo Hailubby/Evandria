cdefc9f4277797ecc501518c9dfc2e2e3235af4a
Kelvin Lau <kclau95@gmail.com>
2016-10-05 23:36:18 +1300

    Added HeathBarScript

---
+++ Assets/Scripts/HealthBarScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour
+{
+
+    public Slider healthBar;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Health is from values 0 to 100 integer only
+    /// </summary>
+    /// <param name="value"></param>
+    public void UpdateHealth(int value)
+    {
+        if (value < 0 || value > 100)
+            throw new System.Exception("value for the HealthBar must be integer from 0 to 100");
+
+        healthBar.value = value;
+    }
+}
