34521ebdaddeab7f28bbc95f2033e99620c1077b
Kelvin Lau <kclau95@gmail.com>
2016-10-05 23:45:07 +1300

    Added LocationTextScript for updating the location text in HUD

---
+++ Assets/Scripts/LocationTextScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LocationTextScript : MonoBehaviour
+{
+
+    public Text locationText;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void UpdateLocationText(string location)
+    {
+        locationText.text = location;
+    }
+}
