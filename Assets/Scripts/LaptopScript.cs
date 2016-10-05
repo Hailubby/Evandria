5b22ce5bc26f41788e578eafa9dce3248e4d9158
OphielOM <wlin943@aucklanduni.ac.nz>
2016-10-05 23:50:33 +1300

    Implemented Laptop to Decision Screen functionality
    Added in a laptop object in order to get to final Decision Screen.
    
    Added in extra "Investigate" button on final decision screen
    
    Added extra scripts to interact with laptop and exit decision screen

---
+++ Assets/Scripts/LaptopScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class LaptopScript : MonoBehaviour, Assets.Scripts.Interactable
+{
+    public MovementScript player;
+    public GameObject decisionCanvas;
+
+    public void interact()
+    {
+        decisionCanvas.SetActive(true);
+        player.DisableMovement();
+    }
+}
