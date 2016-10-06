432b3def1cf357b233ea332ff889eb9403c4dcee
Benjamin Collins <Bcol602@aucklanduni.ac.nz>
2016-10-06 19:58:17 +1300

    Create storage for Chosen Candidate and good/bad outcome

--- Assets/Scripts/EvandriaUpdate.cs
+++ Assets/Scripts/EvandriaUpdate.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class EvandriaUpdate : MonoBehaviour {
 
     HealthBarScript sliderScript;
+    public Candidate chosenCandidate;
+    public bool goodOutcome;
+
     void Start()
     {
         sliderScript = FindObjectOfType<HealthBarScript>();
@@ -13,11 +16,18 @@ public class EvandriaUpdate : MonoBehaviour {
     void DecisionMade (Candidate candidate)
     {
         int changeInMorale = 0;
+        chosenCandidate = candidate;
 
         //Retrieve decision stats for candidate
         //5 stats good/bad values placed in array in order
         int[] goodArray = new int[5]; //Replace with retrieving array from candidate
         int[] badArray = new int[5];
+        int bestCase =0;
+
+        foreach (int x in goodArray)
+        {
+            bestCase += x;
+        }
                 
 
         //Retrieve probability for candidate (e.g: 80/20)
@@ -36,8 +46,18 @@ public class EvandriaUpdate : MonoBehaviour {
             }
         }
 
+        if (bestCase / 2 > changeInMorale)
+        {
+            goodOutcome = false;
+        } else
+        {
+            goodOutcome = true;
+        }
+
 
         //Call UpdateSlider on morale slider with input changeInMorale
         sliderScript.UpdateHealth(changeInMorale);
+
+        
     }
 }
