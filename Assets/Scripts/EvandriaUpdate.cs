800bd15155d34062a5b2ef7b81709fd78c16f14f
Benjamin Collins <Bcol602@aucklanduni.ac.nz>
2016-10-06 19:49:18 +1300

    Evandria update script

---
+++ Assets/Scripts/EvandriaUpdate.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EvandriaUpdate : MonoBehaviour {
+
+    HealthBarScript sliderScript;
+    void Start()
+    {
+        sliderScript = FindObjectOfType<HealthBarScript>();
+    }
+
+	
+    void DecisionMade (Candidate candidate)
+    {
+        int changeInMorale = 0;
+
+        //Retrieve decision stats for candidate
+        //5 stats good/bad values placed in array in order
+        int[] goodArray = new int[5]; //Replace with retrieving array from candidate
+        int[] badArray = new int[5];
+                
+
+        //Retrieve probability for candidate (e.g: 80/20)
+        int badProbability = 0;
+
+        
+        int i;
+        for (i=0; i<5; i++)
+        {
+            if( Random.Range(0f,1f) <= badProbability)
+            {
+                changeInMorale += badArray[i];
+            } else
+            {
+                changeInMorale += goodArray[i];
+            }
+        }
+
+
+        //Call UpdateSlider on morale slider with input changeInMorale
+        sliderScript.UpdateHealth(changeInMorale);
+    }
+}
--- Assets/Scripts/TextReader.cs
+++ Assets/Scripts/TextReader.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class TextReader : MonoBehaviour {
 
+    public TextAsset textFile;
+    public string outcomeText;
+
+    
+
 	// Use this for initialization
 	void Start () {
-	
+	    if (textFile != null)
+        {
+            outcomeText = textFile.text;
+        }
 	}
 	
 	// Update is called once per frame
