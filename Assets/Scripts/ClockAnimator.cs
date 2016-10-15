58f7b77ee85a1c301a450c71c58027964b604d96
Hailubby <hwan561@aucklanduni.ac.nz>
2016-10-15 22:04:05 +1300

    Scaled time to move 8 hours on clock in 12 minutes. Starting position of clock still based on local time.

--- Assets/Scripts/ClockAnimator.cs
+++ Assets/Scripts/ClockAnimator.cs
@@ -6,11 +6,11 @@ public class ClockAnimator : MonoBehaviour {
 
     public Transform hours;
     public Transform minutes;
-    public Transform seconds;
+    //public Transform seconds;
 
-    private const float hoursToDegrees = 360f / 12f;
-    private const float minsToDegrees = 360f / 60f;
-    private const float secsToDegrees = 360f / 60f;
+    private const float hoursToDegrees = (360f / 12f)*(2f/3f);
+    private const float minsToDegrees = (360f / 60f)*(2f/3f);
+    // const float secsToDegrees = 360f / 60f;
 
     // Use this for initialization
     void Start () {
@@ -21,8 +21,8 @@ public class ClockAnimator : MonoBehaviour {
 	void Update () {
         TimeSpan timespan = DateTime.Now.TimeOfDay;
         DateTime time = DateTime.Now;
-        hours.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.TotalHours * -hoursToDegrees);
-        minutes.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.TotalMinutes * -minsToDegrees);
-        seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -secsToDegrees);
+        hours.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.TotalMinutes * -hoursToDegrees);
+        minutes.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.TotalSeconds * -minsToDegrees);
+        //seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -secsToDegrees);
     }
 }
