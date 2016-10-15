7d794012b37ff8031df394f12855943b54fdcf81
Hailubby <hwan561@aucklanduni.ac.nz>
2016-10-15 15:25:40 +1300

    Now working with real time

--- Assets/Scripts/ClockAnimator.cs
+++ Assets/Scripts/ClockAnimator.cs
@@ -8,8 +8,6 @@ public class ClockAnimator : MonoBehaviour {
     public Transform minutes;
     public Transform seconds;
 
-    public bool analog;
-
     private const float hoursToDegrees = 360f / 12f;
     private const float minsToDegrees = 360f / 60f;
     private const float secsToDegrees = 360f / 60f;
@@ -21,21 +19,10 @@ public class ClockAnimator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (analog)
-        {
-            TimeSpan timespan = DateTime.Now.TimeOfDay;
-            DateTime time = DateTime.Now;
-            hours.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.TotalHours * -hoursToDegrees);
-            minutes.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.TotalMinutes * -minsToDegrees);
-            seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -secsToDegrees);
-        }
-        else
-        {
-            DateTime time = DateTime.Now;
-            hours.localRotation = Quaternion.Euler(0f, 0f, time.Hour * -hoursToDegrees);
-            minutes.localRotation = Quaternion.Euler(0f, 0f, time.Minute * -minsToDegrees);
-            seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -secsToDegrees);
-        }
-        
-	}
+        TimeSpan timespan = DateTime.Now.TimeOfDay;
+        DateTime time = DateTime.Now;
+        hours.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.TotalHours * -hoursToDegrees);
+        minutes.localRotation = Quaternion.Euler(0f, 0f, (float)timespan.TotalMinutes * -minsToDegrees);
+        seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -secsToDegrees);
+    }
 }
