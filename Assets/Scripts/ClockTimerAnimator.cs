4429602ba644e3d70a592b9788e118c728b57366
Hailubby <hwan561@aucklanduni.ac.nz>
2016-10-16 00:42:33 +1300

    Combined timer and clock logic into one script.

--- Assets/Scripts/ClockTimerAnimator.cs
+++ Assets/Scripts/ClockTimerAnimator.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System;
 
-public class ClockAnimator : MonoBehaviour {
+public class ClockTimerAnimator : MonoBehaviour {
 
+    public Text timerText;
     public Transform hours;
     public Transform minutes;
     public int level;
@@ -12,11 +14,13 @@ public class ClockAnimator : MonoBehaviour {
     private float minsToDegrees;
 
     private TimeSpan myTimeSpan;
+    private float timer;
 
     // Use this for initialization
     void Start () {
         if (level == 1)
         {
+            timer = 720f;
             myTimeSpan = new TimeSpan(0, 0, 18, 0, 0);
 
             hoursToDegrees = (360f / 12f) * (2f / 3f);
@@ -24,12 +28,14 @@ public class ClockAnimator : MonoBehaviour {
         }
         else if (level == 2)
         {
+            timer = 600f;
             myTimeSpan = new TimeSpan(0, 0, 15, 0, 0);
 
             hoursToDegrees = (360f / 12f) * (4f / 5f);
             minsToDegrees = (360f / 60f) * (4f / 5f);
         }
         else if (level == 3) {
+            timer = 480f;
             myTimeSpan = new TimeSpan(0, 0, 12, 0, 0);
 
             hoursToDegrees = (360f / 12f);
@@ -39,7 +45,29 @@ public class ClockAnimator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //TimeSpan timespan = DateTime.Now.TimeOfDay;
+        //update timer
+        Timer();
+        //update clock hands
+        Clock();
+
+    }
+
+    public void Timer() {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            timer = 0;
+            //Go back to office here
+        }
+
+        string minutes = ((int)timer / 60).ToString();
+        string seconds = (timer % 60).ToString("f1");
+
+        timerText.text = minutes + ":" + seconds;
+    }
+
+    public void Clock() {
         myTimeSpan += TimeSpan.FromSeconds(Time.deltaTime);
         DateTime time = DateTime.Now;
         hours.localRotation = Quaternion.Euler(0f, 0f, (float)myTimeSpan.TotalMinutes * -hoursToDegrees);
