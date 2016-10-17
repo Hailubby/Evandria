dffd482ab180eca9ba19ee84b5426d14d2ab5261
John Ramirez <jc_ramz@hotmail.com>
2016-10-18 12:49:13 +1300

    Deleted NPCWalking.cs file, edited NPCWalk.cs to move the NPC - currently slightly too fast.

--- Assets/Scripts/NPCWalk.cs
+++ Assets/Scripts/NPCWalk.cs
@@ -11,57 +11,41 @@ public class NPCWalk : MonoBehaviour {
 		rigidBody = GetComponent<Rigidbody2D> ();
 		anim = GetComponent<Animator> ();
 
-		InvokeRepeating ("Walk", 2.0f, 1.0f);
+		//InvokeRepeating ("Walk", 2.0f, 1.0f);
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 
-		//Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-
-		/*
-		if (movement_vector != Vector2.zero) {
-			anim.SetBool("isWalking", true);
-			anim.SetFloat("input_x", movement_vector.x);
-			anim.SetFloat("input_y", movement_vector.y);
-		} else {
-			anim.SetBool("isWalking", false);
-		}
-
-		rigidBody.MovePosition(rigidBody.position + movement_vector * Time.deltaTime);
-		*/
-		}
-
-	void Walk () {
 		System.Random rnd = new System.Random ();
 		Vector2 movement_vector = new Vector2 (0, 0);
 
 		int direction = rnd.Next (8);
 
 		switch (direction) {
-			case 0:
-				Debug.Log ("Npc is walking up.");
-				movement_vector = new Vector2 (0, 1);
-				break;
+		case 0:
+			Debug.Log ("Npc is walking up.");
+			movement_vector = new Vector2 (0, 1);
+			break;
 
-			case 1:
-				Debug.Log ("NPC is walking down.");
-				movement_vector = new Vector2 (0, -1);
-				break;
+		case 1:
+			Debug.Log ("NPC is walking down.");
+			movement_vector = new Vector2 (0, -1);
+			break;
 
-			case 2:
-				Debug.Log ("NPC is walking left.");
-				movement_vector = new Vector2 (-1, 0);
-				break;
+		case 2:
+			Debug.Log ("NPC is walking left.");
+			movement_vector = new Vector2 (-1, 0);
+			break;
 
-			case 3:
-				Debug.Log ("NPC is walking right.");
-				movement_vector = new Vector2 (1, 0);
-				break;
-			default:
-				Debug.Log("NPC is staying still.");
-				movement_vector = new Vector2 (0 , 0);
-				break;
+		case 3:
+			Debug.Log ("NPC is walking right.");
+			movement_vector = new Vector2 (1, 0);
+			break;
+		default:
+			Debug.Log("NPC is staying still.");
+			movement_vector = new Vector2 (0 , 0);
+			break;
 		}
 
 		if (movement_vector != Vector2.zero) {
@@ -73,5 +57,9 @@ public class NPCWalk : MonoBehaviour {
 		}
 
 		rigidBody.MovePosition (rigidBody.position + movement_vector * Time.deltaTime);
-	}
+		}
+
+//	void Walk () {
+
+//	}
 }
