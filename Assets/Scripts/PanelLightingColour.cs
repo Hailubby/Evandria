a43e2f098e221ff6abee250eef43379b8b260fed
Hailubby <hwan561@aucklanduni.ac.nz>
2016-10-17 23:45:10 +1300

    Fixed colour change time calculation. Resets on each level rather than continueing now.

--- Assets/Scripts/PanelLightingColour.cs
+++ Assets/Scripts/PanelLightingColour.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System;
 
 public class PanelLightingColour : MonoBehaviour
 {
@@ -21,18 +22,36 @@ public class PanelLightingColour : MonoBehaviour
     // How many seconds to wait before Lerping further                          
     public float _transitionSpeedDelay = 0.05f;
     // 720 seconds in level 1                   
-    public int _dayLength = 720;
+    public int _dayLength;
     // When to trigger the color switches                                                   
-    public int[] _timeList = new int[] { 10, 20, 30, 40 };
+    public int[] _timeList;
     // What color to become
     // The first color is the default color, colour list must be 1 larger than time list.
     public Color[] _colorList = new Color[] { Color.white, Color.white, Color.white, Color.white, Color.white };
 
     private Image img;
+    private TimeSpan myTimeSpan;
 
     // Use this for initialization
     void Start()
     {
+        if (EvandriaUpdate.level == 1)
+        {
+            _dayLength = 720;
+            _timeList = new int[] { 5, 10, 20, 30 };
+        }
+        else if (EvandriaUpdate.level == 2)
+        {
+            _dayLength = 600;
+            _timeList = new int[] { 10, 20, 30, 40 };
+        }
+        else if (EvandriaUpdate.level == 3)
+        {
+            _dayLength = 480;
+            _timeList = new int[] { 15, 30, 45, 60 };
+        }
+
+        myTimeSpan = new TimeSpan(0, 0, 0, 0, 0);
         img = lightingPanel.GetComponent<Image>();
         img.color = new Color32(0xFF, 0x00, 0x00, 0x2F);
 
@@ -73,7 +92,9 @@ public class PanelLightingColour : MonoBehaviour
             lightingPanel.SetActive(false);
         }
 
-        float curTime = Time.fixedTime % PanelLightingColour.dayLength; // The current time of day is the number of seconds since start mod the day length in seconds
+        myTimeSpan += TimeSpan.FromSeconds(Time.deltaTime);
+        float curTime = (float)myTimeSpan.TotalSeconds % PanelLightingColour.dayLength;
+        //float curTime = Time.fixedTime % PanelLightingColour.dayLength; // The current time of day is the number of seconds since start mod the day length in seconds
 
         if (coloursLeft)
         { // We're still within the time list bounds
