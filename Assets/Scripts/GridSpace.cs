6e1aac2f06182087288ee1b2fcbea8e2d6507b22
Amy <xlin504@aucklanduni.ac.nz>
2016-10-19 19:13:21 +1300

    Script for setting up reference to GameController and initiating the board

---
+++ Assets/Scripts/GridSpace.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+// Source: https://unity3d.com/learn/tutorials/tic-tac-toe/foundation-game-play?playlist=17111
+public class GridSpace : MonoBehaviour {
+
+    public Button button;
+    public Text buttonText;
+    // Don't need playerSide anymore, this value is now taken directly from the Game Controller
+    // public string playerSide;
+
+    // Hold a reference to our tic tac toe game controller
+    private TicTacToeGameController gameController;
+
+    public void SetSpace()
+    {
+        buttonText.text = gameController.GetPlayerSide();
+        button.interactable = false;
+        gameController.EndTurn();
+    }
+
+    // Takes a GameController as input and assign it to the gameController variable as a reference.
+    public void SetGameControllerReference(TicTacToeGameController controller)
+    {
+        gameController = controller;
+    }
+
+}
