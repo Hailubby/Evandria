19a1b05f070b5812d54e454aa14a614f278e6c88
OphielOM <wlin943@aucklanduni.ac.nz>
2016-10-19 15:54:10 +1300

    Editted filler spawn locations to actual spawn locations

--- Assets/Scripts/Locations.cs
+++ Assets/Scripts/Locations.cs
@@ -8,11 +8,11 @@ public class Locations : MonoBehaviour {
     // Use this for initialization
     void Start() {
         //hard coded filler vectors for location spawn points
-        Vector3[] officeSpawn = { new Vector3(0, 0, 0), new Vector3(1, 1, 0), new Vector3(2, 2, 0) };
-        Vector3[] medium_1Spawn = { new Vector3(5, 5, 0), new Vector3(10, 10, 0), new Vector3(15, 15, 0), new Vector3(20, 20, 0), new Vector3(25, 25, 0) };
-        Vector3[] small_1Spawn = { new Vector3(-5, -5, 0), new Vector3(-10, -10, 0), new Vector3(-15, -15, 0), new Vector3(-20, -20, 0), new Vector3(-25, -25, 0) };
-        Vector3[] medium_2Spawn = { new Vector3(100, 100, 0), new Vector3(110, 110, 0), new Vector3(120, 120, 0), new Vector3(130, 130, 0), new Vector3(140, 140, 0) };
-        Vector3[] small_2Spawn = { new Vector3(-100, -100, 0), new Vector3(-110, -110, 0), new Vector3(-120, -120, 0), new Vector3(-130, -130, 0), new Vector3(-140, -140, 0) };
+        Vector3[] officeSpawn = { new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(0, 0, 0) };
+        Vector3[] medium_1Spawn = { new Vector3(110.3f, 1.1f, 0), new Vector3(113.6f, 1.1f, 0), new Vector3(116.8f, 1.1f, 0), new Vector3(72.1f, 23.5f, 0), new Vector3(75.2f, 23.5f, 0) };
+        Vector3[] small_1Spawn = { new Vector3(-86.5f, 16.5f, 0), new Vector3(83.4f, 16.5f, 0), new Vector3(-80.2f, 16, 0), new Vector3(-77, 16, 0), new Vector3(-76.9f, -3, 0) };
+        Vector3[] medium_2Spawn = { new Vector3(95.9f, -91.2f, 0), new Vector3(73.5f, -84.7f, 0), new Vector3(67, -84.7f, 0), new Vector3(131, -75, 0), new Vector3(134.2f, -75, 0) };
+        Vector3[] small_2Spawn = { new Vector3(-102.3f, -73, 0), new Vector3(-99, -73, 0), new Vector3(-95.8f, -73, 0), new Vector3(-108.5f, -73, 0), new Vector3(-112, -73, 0) };
 
         locations = new ArrayList();
         locations.Add(new Location(new Vector3(-13, -5, 0), "Office", officeSpawn));
