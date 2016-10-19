3ed7bfb6f1e769d92eb604f34bd2c05de657e22e
John Ramirez <jc_ramz@hotmail.com>
2016-10-19 13:15:49 +1300

    Created preliminary object spawning script.

---
+++ Assets/Scripts/ObjectSpawning.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSpawning : MonoBehaviour {
+
+	//GameObject[] objects;
+	GameObject currentObject;
+	Vector3[] locations;
+	bool[] isPlaced;
+	int counter = 0;
+
+	// Use this for initialization
+	void Start () {
+		//objects = new GameObject[2];
+		locations = new Vector3[4];
+		isPlaced = new bool[4];
+		currentObject = GameObject.Find ("Axe");
+
+		for (int i = 0; i < isPlaced.Length; i++) {
+			if (i == 2 || i == 3) {
+				isPlaced [i] = false;
+			} else {
+				isPlaced [i] = true;
+			}
+		}
+
+		//Commented out the locations
+
+		locations [0] = new Vector3 (5, 0, 0);
+		locations [1] = new Vector3 (-5, 0, 0);
+		locations [2] = new Vector3 (0, 5, 0);
+		locations [3] = new Vector3 (0, -5, 0);
+
+		System.Random rnd = new System.Random();
+
+		while (counter < 1) {
+			int place = rnd.Next(4);
+
+			if (!isPlaced [place]) {
+				currentObject.transform.position = locations [place];
+				isPlaced [place] = true;
+				counter++;
+			} else {
+				Debug.Log ("Currently isPlaced[place] = true and place = " + place);
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
