7d998ae3eabe38c098419f0fa1c277f6bf8c4c91
Xiaohui Lin <xlin504@aucklanduni.ac.nz>
2016-10-19 15:34:41 +1300

    Store a list of theme variations. Also includes methods for serialization and deserialization between XML and C# Objects

---
+++ Assets/Scripts/ThemeContainer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+[XmlRoot(ElementName = "ThemeCollection")]
+public class ThemeContainer {
+
+    [XmlArray("Themes"), XmlArrayItem("Theme")]
+    //public List<Theme> Themes = new List<Theme>();
+    public Theme[] Themes;
+
+    public void Save(string path)
+    {
+        var serializer = new XmlSerializer(typeof(ThemeContainer));
+        using (var stream = new FileStream(path, FileMode.Create))
+        {
+            serializer.Serialize(stream, this);
+        }
+    }
+
+    public static ThemeContainer Load(string path)
+    {
+        var serializer = new XmlSerializer(typeof(ThemeContainer));
+        using (var stream = new FileStream(path, FileMode.Open))
+        {
+            return serializer.Deserialize(stream) as ThemeContainer;
+        }
+    }
+
+    //Loads the xml directly from the given string. Useful in combination with www.text.
+    public static ThemeContainer LoadFromText(string text)
+    {
+        var serializer = new XmlSerializer(typeof(ThemeContainer));
+        return serializer.Deserialize(new StringReader(text)) as ThemeContainer;
+    }
+}
