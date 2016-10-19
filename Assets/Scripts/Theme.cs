622d0182dd37f84d24c6c07977162a3cf8c3b8be
Xiaohui Lin <xlin504@aucklanduni.ac.nz>
2016-10-19 15:32:53 +1300

    Added a Theme object model

---
+++ Assets/Scripts/Theme.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+
+// A model of a theme instance
+public class Theme {
+
+    [XmlAttribute("selected")]
+    public bool Selected;
+    public string Name;
+    public string FontColour;
+    public string BGColour;
+
+}
