a278fc951d8f75fe594be9c112108a153e387078
Xiaohui Lin <xlin504@aucklanduni.ac.nz>
2016-10-19 15:36:47 +1300

    Read from xml file and change theme accordingly for the HUD panel

---
+++ Assets/Scripts/ChangeTheme.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChangeTheme : MonoBehaviour {
+
+    public GameObject TopPanel;
+
+	// Use this for initialization
+	void Start () {
+
+        // Read if light or dark theme and change colours accordingly
+        GlobalConfigSettings settings = GameObject.FindObjectOfType<GlobalConfigSettings>();
+        Image img =  TopPanel.GetComponent<Image>();
+        string bgColorHex = null;
+        string fontColorHex = null;
+
+        // Get the active hex value
+        foreach (Theme theme in settings.themeContainer.Themes)
+        {
+            if (theme.Selected)
+            {
+                bgColorHex = theme.BGColour;
+                fontColorHex = theme.FontColour;
+                break;
+            }
+        }
+
+        // Set the background colour 
+        Color bgColor = new Color();
+        ColorUtility.TryParseHtmlString(bgColorHex, out bgColor);
+        img.color = bgColor;
+
+        // Set the font color
+        GameObject canvasObject = GameObject.Find("HUDCanvas/TopPanel");
+
+        Color fontColor = new Color();
+        ColorUtility.TryParseHtmlString(fontColorHex, out fontColor);
+
+        Debug.Log(canvasObject.transform.childCount);
+
+        for (int i = 0; i < canvasObject.transform.childCount; i++)
+        {
+            GameObject child = canvasObject.transform.GetChild(i).gameObject;
+            if (child.transform.childCount == 1 || child.transform.childCount == 2)
+            {
+                child.transform.GetChild(0).gameObject.GetComponent<Text>().color = fontColor;
+            }
+            else if (child.transform.childCount == 0)
+            {
+                Text targetText = child.GetComponent<Text>();
+                targetText.color = fontColor;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
