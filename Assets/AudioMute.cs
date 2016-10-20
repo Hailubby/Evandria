a5cbe8b770de1f1fd460f41981888396f2e44aa7
Fraser Lewis-Smith <lewissmith.fraser@gmail.com>
2016-10-21 01:56:22 +1300

    Music & Mute

---
+++ Assets/AudioMute.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioMute : MonoBehaviour {
+
+    bool isMute;
+
+    public void Mute()
+    {
+        isMute = !isMute;
+        AudioListener.volume = isMute ? 0 : 1;
+    }
+}
