27610d85d59c4c17d6f034aa0cdc2a6c10adca1d
Xiaohui Lin <xlin504@aucklanduni.ac.nz>
2016-10-20 16:16:40 +1300

    Drag and drop puzzle to solve Tara's ripped up exam timetable

---
+++ Assets/Scripts/ContinueAfterPuzzleCompleted.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ContinueAfterPuzzleCompleted : MonoBehaviour {
+
+    public Button continueButton;
+
+    // Use this for initialization
+    //void Start () {
+     //   continueButton.gameObject.SetActive(false);
+    //}
+
+	public void ShowButton () {
+        continueButton.gameObject.SetActive(true);
+    }
+}
